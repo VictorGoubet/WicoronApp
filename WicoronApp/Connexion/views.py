1976d6f835ff3414040d3d3c1db835df0a40daa4
Boris LOUCHART <boris.louchart@edu.devinci.fr>
2020-05-02 17:20:14 +0200

    Ajout utilisateur page signup

--- WicoronApp/Connexion/views.py
+++ WicoronApp/Connexion/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def Register(request):
+    if( request.method=='POST'):
+        form = SignUpForm(request.POST)
+        if(form.is_valid()):
+            user = form.save()
+            user.refresh_from_db()
+
+            #Puis sign in
+            return render(request,"Connexion/signin.html")
+    else:
+        form = SignUpForm()
     return render(request,"Connexion/signup.html")
 
 
