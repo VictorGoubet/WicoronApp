28897b9e0a7a738cd1333581e9f29e5a3220ab95
Victor Goubet <victor.goubet@edu.devinci.fr>
2020-05-07 15:32:06 +0200

    slash

--- WicoronApp/settings.py
+++ WicoronApp/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'WicoronApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates').replace('\\','/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
