39f43cf27cdbbb34d968244244d2190a2602beb2
Victor Goubet <victor.goubet@edu.devinci.fr>
2020-05-07 20:22:42 +0200

    modif settings heroku

--- WicoronApp/prod_settings.py
+++ WicoronApp/prod_settings.py
@@ -10,6 +10,19 @@ ALLOWED_HOSTS = ['wicoronapp.herokuapp.com']
 
 MIDDLEWARE+=['whitenoise.middleware.WhiteNoiseMiddleware',]
 
+TEMPLATES=[{
+    'APP_DIRS': True,
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': ['templates'],
+    'OPTIONS': {'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+        'social_django.context_processors.backends',
+        'social_django.context_processors.login_redirect']}
+        }]
+
 
 
 SECRET_KEY = os.environ['SECRET_KEY']
