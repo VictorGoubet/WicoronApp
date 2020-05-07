e6785ee86ff221191fc214968432d96195ef3824
Victor Goubet <victor.goubet@edu.devinci.fr>
2020-05-07 14:15:17 +0200

    Ajout de whitenoise pour la delivration CDn des fichiers statics

--- WicoronApp/prod_settings.py
+++ WicoronApp/prod_settings.py
@@ -6,7 +6,10 @@ DEBUG = False
 DATABASES['default']=dj_database_url.config()
 
 ALLOWED_HOSTS = ['wicoronapp.herokuapp.com']
+
+MIDDLEWARE+=['whitenoise.middleware.WhiteNoiseMiddleware',]
 TEMPLATE_DEBUG=True
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 SECRET_KEY = 'j5)ti68(p_j30u$dtl0bm6xbhn0vkzi^q05--ejf*97vu)$fe1'
